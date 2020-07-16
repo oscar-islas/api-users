9721f7cb402aa175cb54853b6777a155b8cce947
Oscar Islas <oislasreyes@gmail.com>
2020-07-16 18:09:58 -0500

    Agregando el endpoint para actualizar a los usuarios

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/users', [
 ], (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        res.status(400).json(errors)
+        res.status(400).json({message: "Hay campos invalidos", errors})
     }else{
         let user = {...req.body};
         let users =  [];
@@ -95,6 +95,44 @@ app.delete('/user/:id', (req, res) => {
             }
         }
     });
-})
+});
+
+app.put('/user/:id', [
+    check('name').isLength({min: 1}),
+    check('lastname').isLength({min: 1}),
+    check('email', 'El email ingresado es invalido').isEmail(),
+    check('password', 'La contraseña debe de tener una longitud minima de 6 caracteres').isLength({min: 6}),
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message: "Hay campos invalidos", errors});
+    }
+    let id = Number(req.params.id);    
+    let users =  [];
+    fs.readFile(path.join(__dirname, 'users.json'), (error, data) => {
+        if(error){
+            res.status(400).json({
+                message: "No se ha podido leer la base de datos"
+            });
+        }else{
+            users = JSON.parse(data.toString());
+            let position = users.findIndex(user => user.id === id);
+            let user = users.find(user => user.id === id);
+            user = {['id']: user.id, ...req.body};   
+            users[position] = user;         
+            if(position>=0){
+                fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users), (error) => {
+                    if(error){
+                        res.status(400).json({error: 'Hubo un error al actualizar el usuario en el sistema'});
+                    }else{
+                        res.status(200).json({message: 'El usuario ha sido actualizado correctamente', data: user });
+                    }
+                });
+            }else{
+                res.status(400).json({error: 'No existe el usuario en el sistema'});
+            }
+        }
+    });
+});
 
 app.listen(port, () => console.log("servidor escuchando sobre el puerto 8000"));
